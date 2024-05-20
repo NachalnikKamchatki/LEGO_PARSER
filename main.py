22ee39d2ca0e7fd1e52f52bc9be791f2edf67012
NachalnikKamchatki <gonzito1981@gmail.com>
2024-05-20 23:32:05 +0300

    pagination and some methods

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ url_main = 'https://www.lego.com'
 url_themes = 'https://www.lego.com/en-us/themes'
 
 
-def get_soup(url):
-    r = requests.get(url=url)
+def get_soup(url, page=1):
+    if page == 1:
+        r = requests.get(url=url)
+    else:
+        url = f'{url}?page={page}&offset=0'
+        r = requests.get(url=url)
     return BeautifulSoup(r.text, 'lxml')
 
 
@@ -34,9 +38,18 @@ def get_themes(soup):
     return themes_list
 
 
+def get_toys_pages(soup):
+    num_toys = int(soup.select('span[data-value]')[0].get('data-value'))
+    num_pages = num_toys // 18 + 1
+
+    return num_toys, num_pages
+
+
 def main():
-    soup = get_soup(url_themes)
-    themes = get_themes(soup)
+    # soup = get_soup(url=url_themes)
+    # themes = get_themes(soup=soup)
+    soup = get_soup(url='https://www.lego.com/en-us/themes/city')
+    print(get_toys_pages(soup=soup))
 
 
 if __name__ == '__main__':
